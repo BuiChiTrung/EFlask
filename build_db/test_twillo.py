3973deee4691c5fcb1b5c6f6eb1a54257d846a12
Bui Chi Trung <trungcspntl@gmail.com>
2022-06-11 23:32:50 +0700

    update env

--- build_db/test_twillo.py
+++ build_db/test_twillo.py
@@ -6,13 +6,15 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC679de3766e3c0c980f4355884d3ba3c5'
-auth_token = '799bead51a964c8696489b2fb35b09d5'
+auth_token = '5b7e3ae704c9f9e67483be8b9d737d55'
+# ACCOUNT_SID=AC679de3766e3c0c980f4355884d3ba3c5
+# AUTH_TOKEN=799bead51a964c8696489b2fb35b09d5
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Em yeu anh',
+                              body='Chao youtuber',
                               from_='+13254221982',
-                              to='+840988864538'
+                              to='+84326921446'
                           )
 
 print(message.sid)

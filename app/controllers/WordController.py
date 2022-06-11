ec879b12dd97a1ff4a6d30de677357e27b7e4884
Bui Chi Trung <trungcspntl@gmail.com>
2022-06-11 23:59:59 +0700

    random words api

--- app/controllers/WordController.py
+++ app/controllers/WordController.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify, request
+import random
+
+from flask import Blueprint, request
 from flask_login import login_required
 
 from app import db
@@ -15,10 +17,7 @@ def find_like():
     result = []
     
     for word in words:
-        defs = word.sys_defs.all()
-        word = word.as_dict()
-        word['sys_defs'] = list_to_json_array(defs)
-        result.append(word)
+        result.append(get_word_defs_and_convert_to_dict(word))
 
     return json_response(True, result)
 
@@ -28,8 +27,30 @@ def show(id):
     if hasattr(word, 'vi_mean') == False:
         repository.update(word.id, {'vi_meaning': google_translate(word.word)})
     
+    word = get_word_defs_and_convert_to_dict(word)
+    return json_response(True, word)
+
+@word_blueprint.route('/random')
+def get_random_words():
+    quantity = request.args.get('quantity')
+    TOTAL_WORDS = 48190
+    
+    if int(quantity) > TOTAL_WORDS:
+        return json_response(False, 'Invalid quantity', 400)
+    
+    random_word_ids = random.sample(range(1, TOTAL_WORDS), int(quantity))
+    
+    result = []
+    for id in random_word_ids:
+        word = repository.show(id)
+        result.append(get_word_defs_and_convert_to_dict(word))
+    
+    return json_response(True, result)
+
+
+def get_word_defs_and_convert_to_dict(word):
     defs = word.sys_defs.all()
     word = word.as_dict()
     word['sys_defs'] = list_to_json_array(defs)
-
-    return json_response(True, word)
+    
+    return word
